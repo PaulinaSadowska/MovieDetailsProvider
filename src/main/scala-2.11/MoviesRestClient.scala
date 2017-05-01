c2e1adc3cbd8d2de89cf5963ca7efc7a65cd5e44
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2017-05-01 15:16:34 +0200

    basic deserialization

--- src/main/scala-2.11/MoviesRestClient.scala
+++ src/main/scala-2.11/MoviesRestClient.scala
@@ -1,6 +1,7 @@
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
+import play.api.libs.json._
 import play.api.libs.ws.WSClient
 import play.api.libs.ws.ahc.AhcWSClient
 
@@ -11,6 +12,8 @@ import scala.concurrent.Future
   * Created by Paulina Sadowska on 01.05.2017.
   */
 object MoviesRestClient {
+
+  case class Movie(adult: Boolean, budget: Int)
 
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
@@ -24,11 +27,19 @@ object MoviesRestClient {
   }
 
   def call(wsClient: WSClient, apiKey: String): Future[Unit] = {
-    val movieId = "150540"
+    val movieId = "680"
     wsClient.url("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey + "&append_to_response=credits").get().map {
       response =>
         val body: String = response.body
+        if (response.status != 200) {
+          println(s"Received unexpected status ${response.status} : ${response.body}")
+        }
         println(s"Got a response $body")
+        val bod: JsValue = Json.parse(body)
+        val adult = (bod \ "adult").as[Boolean]
+        val title = (bod \ "original_title").as[String]
+        val iss = if(adult) " is " else " isn't "
+        println(title + iss + "movie for adults")
     }
   }
 }
