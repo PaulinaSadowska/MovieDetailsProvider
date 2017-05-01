b6521e3bcfb08b968cf24316cd7a6f15f90ef869
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2017-05-01 14:13:11 +0200

    read movie/details response from movieDb

--- src/main/scala-2.11/MoviesRestClient.scala
+++ src/main/scala-2.11/MoviesRestClient.scala
@@ -1,5 +1,6 @@
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 import play.api.libs.ws.WSClient
 import play.api.libs.ws.ahc.AhcWSClient
 
@@ -15,16 +16,19 @@ object MoviesRestClient {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
     val wsClient = AhcWSClient()
+    val apiKey = ConfigFactory.load("application").getString("movieDb.apiKey")
 
-    call(wsClient)
+    call(wsClient, apiKey)
       .andThen { case _ => wsClient.close() }
       .andThen { case _ => system.terminate() }
   }
 
-  def call(wsClient: WSClient): Future[Unit] = {
-    wsClient.url("http://jsonplaceholder.typicode.com/comments/1").get().map { response =>
-      val body: String = response.body
-      println(s"Got a response $body")
+  def call(wsClient: WSClient, apiKey: String): Future[Unit] = {
+    val movieId = "150540"
+    wsClient.url("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey + "&append_to_response=credits").get().map {
+      response =>
+        val body: String = response.body
+        println(s"Got a response $body")
     }
   }
 }
