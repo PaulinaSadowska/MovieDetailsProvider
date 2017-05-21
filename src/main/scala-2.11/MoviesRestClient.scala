25f9960143806667ab1e52a66d8f73059df034cc
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2017-05-21 12:39:41 +0200

    add sequence of items to db

--- src/main/scala-2.11/MoviesRestClient.scala
+++ src/main/scala-2.11/MoviesRestClient.scala
@@ -1,6 +1,7 @@
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
+import data.Movie
 import data.db.MovieData
 import network.ApiHelper
 import play.api.libs.ws.ahc.AhcWSClient
@@ -11,6 +12,8 @@ import scala.concurrent.duration.Duration
 import slick.backend.DatabasePublisher
 import slick.driver.H2Driver.api._
 
+import scala.collection.mutable.ListBuffer
+
 
 /**
   * Created by Paulina Sadowska on 01.05.2017.
@@ -18,6 +21,13 @@ import slick.driver.H2Driver.api._
 object MoviesRestClient {
 
   private val API_KEY_PATH = "movieDb.apiKey"
+
+  def toMovieData(m: Movie) = {
+    (m.id, m.adult, m.budget, m.original_language,
+      m.popularity, m.revenue, m.runtime, m.vote_average,
+      m.vote_count, m.releaseYear, m.directorId, m.firstActorId,
+      m.secondActorId, m.genreIds)
+  }
 
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
@@ -27,36 +37,35 @@ object MoviesRestClient {
     val apiKey = ConfigFactory.load().getString(API_KEY_PATH)
     val allMovieIds = FileLoader.loadMoviesIds()
 
-    val movieIds = allMovieIds.take(1)
-    val movies = ApiHelper.fetchMovies(wsClient, movieIds, apiKey)
     val db = Database.forConfig("sqlite")
     try {
       // The query interface for the Movies table
       val moviesTable = TableQuery[MovieData]
-      val m = movies.head
 
       val deleteMoviesAction: DBIO[Unit] = DBIO.seq(
         moviesTable.delete
       )
 
+      val movieIds = allMovieIds.take(2)
+      val movies = ApiHelper.fetchMovies(wsClient, movieIds, apiKey)
+
+      var moviesSeq: Seq[(Int, Boolean, Int, String, Double, Int,
+        Int, Double, Int, Int, Int, Int, Int, String)] = Seq.empty
+      for (elem <- movies) {
+        moviesSeq :+= toMovieData(elem)
+      }
+      val addMoviesAction: DBIO[Option[Int]] = moviesTable ++= moviesSeq
       val deleteMoviesFuture: Future[Unit] = db.run(deleteMoviesAction)
       val f = deleteMoviesFuture.flatMap { _ =>
-        val addMoviesAction: DBIO[Unit] = DBIO.seq(
-          moviesTable += (m.id, m.adult, m.budget, m.original_language,
-            m.popularity, m.revenue, m.runtime, m.vote_average,
-            m.vote_count, m.releaseYear, m.directorId, m.firstActorId,
-            m.secondActorId, m.genreIds)
-        )
         db.run(addMoviesAction)
       }
       Await.result(f, Duration.Inf)
+      println("\nmovies fetched " + movies.length)
+      println("movies ids on the list " + movieIds.size)
     } finally db.close
 
     wsClient.close()
     system.terminate()
-
-    println("\nmovies fetched " + movies.length)
-    println("movies ids on the list " + movieIds.size)
 
   }
 
